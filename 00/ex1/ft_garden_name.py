8e95c7613616329ed9a1ed1932b9ee6408f4a3f3
1F10250186 <s1f102501866@iniad.org>
2026-09-07 13:15:32 +0900

    ex0 ex1

---
+++ 00/ex1/ft_garden_name.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_name.py                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: rishiyam <rishiyam@student.42tokyo.jp>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/09/07 12:39:09 by rishiyam          #+#    #+#              #
+#    Updated: 2026/09/07 13:12:46 by rishiyam         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_garden_name():
+    name = input("Enter garden name: ")
+    print(f"Garden: {name}")
+    print("Status: Growing well!")
