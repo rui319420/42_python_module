d7e41a985903f0482d388a4ae72b15548471b771
Rui Ishiyama <rishiyam@c5r3s5.42tokyo.jp>
2026-09-10 18:56:35 +0900

    feat: ex2

--- 02/ex1/ft_raise_excetion.py
+++ 02/ex1/ft_raise_excetion.py
@@ -1,12 +1,20 @@
 
 
 def input_temperature(temp_str: str) -> int:
-    return int(temp_str)
+    temp = int(temp_str)
+    if 0 <= temp <= 40:
+        return temp
+    elif temp < 0:
+        raise ValueError(
+            f"{temp}°C is too cold for plants (min 0°C)")
+    else:
+        raise ValueError(
+            f"{temp}°C is too hot for plants (max 40°C)")
 
 
 def test_temperature() -> None:
     print("=== Garden Temperature ===")
-    inputs = ["25", "abc"]
+    inputs = ["25", "abc", "100", "-50"]
     for i in inputs:
         print()
         print(f"Input data is '{i}")
