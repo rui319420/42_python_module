f83c262923b1ad25f8f95079b51575d4d25f8356
1F10250186 <s1f102501866@iniad.org>
2026-09-14 01:35:21 +0900

    fix: ex3

--- 02/ex3/ft_custom_errors.py
+++ 02/ex3/ft_custom_errors.py
@@ -1,17 +1,14 @@
 class GardenError(Exception):
-    def __str__(self) -> str:
-        return "Unknown plant error"
+    def __init__(self, message: str = "Unknown plant error") -> None:
+        super().__init__(message)
 
 
 class PlantError(GardenError):
-    def __str__(self) -> str:
-        return f"The tomato plant is wilting!"
+    pass
 
 
 class WaterError(GardenError):
-    def __str__(self) -> str:
-        super().__str__()
-        return "Not enough water in the tank!"
+    pass
 
 
 def main():
@@ -19,26 +16,26 @@ def main():
     print()
     print("Testing PlantError...")
     try:
-        raise PlantError()
+        raise PlantError("The tomato plant is wilting!")
     except PlantError as e:
         print(f"Caught PlantError: {e}")
 
     print()
     print("Testing WaterError...")
     try:
-        raise WaterError()
+        raise WaterError("Not enough water in the tank!")
     except WaterError as e:
         print(f"Caught WaterError: {e}")
 
     print()
     print("Testing catching all garden errors...")
     try:
-        raise PlantError()
+        raise PlantError("The tomato plant is wilting!")
     except GardenError as e:
         print(f"Caught GardenError: {e}")
 
     try:
-        raise WaterError()
+        raise WaterError("Not enough water in the tank!")
     except GardenError as e:
         print(f"Caught GardenError: {e}")
 
