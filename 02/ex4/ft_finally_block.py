d09be0e11e1bde6f62e2ac2de30a353a769a5025
1F10250186 <s1f102501866@iniad.org>
2026-09-14 02:21:26 +0900

    ex3 and ex4

--- 02/ex4/ft_finally_block.py
+++ 02/ex4/ft_finally_block.py
@@ -1,6 +1,9 @@
 class GardenError(Exception):
     def __init__(self, message: str = "Unknown plant error") -> None:
-        super().__init__(message)
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
 
 
 class PlantError(GardenError):
